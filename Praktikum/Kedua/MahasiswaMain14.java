71f9638903404a2b6b7eb376d79be24b3852b14f
Rayhannn18 <rayhangiri24@gmail.com>
2025-02-19 09:27:11 +0700

    Update MahasiswaMain14.java

--- Praktikum/Kedua/MahasiswaMain14.java
+++ Praktikum/Kedua/MahasiswaMain14.java
@@ -6,5 +6,13 @@ public class MahasiswaMain14 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa14 mhs2 = new Mahasiswa14("Annisa Nabila", "2141720106", "TI 2L", 3.25);
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+
+        Mahasiswa14 mhs3 = new Mahasiswa14("Faiza Novyta", "2141724253", "SI 2D", 3.68);
+        mhs3.updateIpk(3.98);
+        mhs3.tampilkanInformasi();
     }
 }
